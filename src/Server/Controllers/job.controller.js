50e13037baf811382e79e89b284445ed0fe0677f
Kumar Saurav <krsaurav1007@gmail.com>
2021-11-14 18:30:14 +0530

    wrote apis

--- src/Server/Controllers/job.controller.js
+++ src/Server/Controllers/job.controller.js
@@ -1,4 +1,23 @@
 const express = require("express")
+
+const Job = require("../Models/job.model")
+
 const router = express.Router()
 
 // get all jobs list
+
+router.get("", async (req, res) => {
+  const title = req.query.title || ""
+  const location = req.query.location || ""
+  const query = { title, location }
+  const jobs = await Job.find(query).lean().exec()
+  return res.status(200).json({ jobs })
+})
+
+// get job of a particular id
+router.get("/:id", async (req, res) => {
+  const job = await Job.findById(req.params.id).lean().exec()
+  return res.status(200).json({ job })
+})
+
+module.exports = router
--- src/Server/server.js
+++ src/Server/server.js
@@ -5,7 +5,11 @@ const connect = require("./Configs/db")
 const app = express()
 app.use(express.json())
 
+const jobsController = require("./Controllers/job.controller")
+
+app.use("/jobs", jobsController)
+
 app.listen(2500, async function () {
   await connect()
-  console.log("listening on port 2345")
+  console.log("listening on port 2500")
 })

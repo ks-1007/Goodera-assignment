244b6a4de61884a13fe4ec282ae0f4b516016133
Kumar Saurav <krsaurav1007@gmail.com>
2021-11-14 16:45:57 +0530

    navbar completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import logo from "./logo.svg"
 import "./App.css"
+import Navbar from "./Components/Navbar/Navbar"
+import ExplorePage from "./Pages/ExplorePage"
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <Navbar />
+      <ExplorePage />
+    </div>
+  )
 }
 
 export default App
---
+++ src/Pages/ExplorePage.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+import styles from "../Components/ExplorePageComponents/ExplorePage.module.css"
+function ExplorePage() {
+  return <div></div>
+}
+
+export default ExplorePage

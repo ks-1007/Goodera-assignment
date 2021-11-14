0909ecc94d65d8afe4d74dfa00b88391b0562f7c
Kumar Saurav <krsaurav1007@gmail.com>
2021-11-14 17:56:56 +0530

    made ui of job list section

--- src/Components/ExplorePageComponents/JobCard.jsx
+++ src/Components/ExplorePageComponents/JobCard.jsx
@@ -1,7 +1,17 @@
 import React from "react"
 import styles from "./JoblistSection.module.css"
 function JobCard({ title, image, description }) {
-  return <div className={styles.jobCardRoot}></div>
+  return (
+    <div className={styles.jobCardRoot}>
+      <div className={styles.imgBox}>
+        <img src={image} alt="job" />
+      </div>
+      <div className={styles.textBox}>
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  )
 }
 
 export default JobCard
